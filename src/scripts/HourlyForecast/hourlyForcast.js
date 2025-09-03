c38e07b41d3b0a9ba3633e583528eb1df5ae7e25
“Nik <nikolaspreyes@gmail.com>
2025-09-03 15:30:30 -0700

    "bug(fix): resolve issue of content jumping content
    -  when pressing forward/back buttons page would would jump so the
    carousel appeared at top of screen. Use scrollIntoView({ block:
    'nearest' } to eliminate this issue

--- src/scripts/HourlyForecast/hourlyForcast.js
+++ src/scripts/HourlyForecast/hourlyForcast.js
@@ -1,5 +1,7 @@
 import './hourly-forecast.css';
 
+//see description of { block: 'nearest' } here: https://stackoverflow.com/a/48635751/628418
+
 export default function makeHours() {
 	const hourCards = Array.from(document.querySelectorAll('.hour-card'));
 	const carousel = document.querySelector('.carousel');
@@ -9,14 +11,14 @@ export default function makeHours() {
 		const numberOfCards = Math.round(carousel.scrollLeft / cardWidth);
 
 		if (numberOfCards - 2 <= 0) {
-			hourCards[0].scrollIntoView();
+			hourCards[0].scrollIntoView({ block: 'nearest' });
 			return;
 		}
 
 		const decimalPortion = parseFloat('0.' + (carousel.scrollLeft / cardWidth + '').split(/\D/)[1]);
 		decimalPortion >= 0.6
-			? hourCards[numberOfCards - 2].scrollIntoView()
-			: hourCards[numberOfCards - 1].scrollIntoView();
+			? hourCards[numberOfCards - 2].scrollIntoView({ block: 'nearest' })
+			: hourCards[numberOfCards - 1].scrollIntoView({ block: 'nearest' });
 	}
 
 	function shiftCarouselForward(cardWidth) {
@@ -25,14 +27,15 @@ export default function makeHours() {
 		const wholeNumber = parseFloat((numberOfCards + '').split(/\D/).at(0));
 
 		if (wholeNumber >= hourCards.length - 1) {
-			hourCards[hourCards.length - 1].scrollIntoView();
+			hourCards[hourCards.length - 1].scrollIntoView({ block: 'nearest' });
 			return;
 		} else {
-			hourCards.at(wholeNumber + 1).scrollIntoView();
+			hourCards.at(wholeNumber + 1).scrollIntoView({ block: 'nearest' });
 		}
 	}
 
 	function handleCarouselDirectionClicks(e) {
+		e.preventDefault();
 		const target = e.target.closest('.carousel-btn');
 		if (target === null) return;
 
