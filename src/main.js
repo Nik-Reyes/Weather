d6f15b2ad2d45592c45228cbf61b140b74821d5e
“Nik <nikolaspreyes@gmail.com>
2025-09-03 19:14:29 -0700

    refactor: add realtime hours to hour carousel

--- src/main.js
+++ src/main.js
@@ -4,5 +4,14 @@ import circularReadout from './scripts/CircularReadout/circularReadout.js';
 import loadCurrentForcast from './scripts/CurrentForecast/currentForecast.js';
 import tenDayForecast from './scripts/TenDayForecast/tenDayForecast.js';
 
+// circularReadout();
+const pageWrapper = document.querySelector('.page-wrapper');
+
+//ask weather data service to get the weather data
+// import WeatherData from './modules/WeatherDataService.js';
+// const weatherData = new WeatherData();
+// await weatherData.fetchWeatherData();
+// console.log(weatherData.rawData);
+
+// create hourly forecast
 makeHours();
-circularReadout();
--- src/scripts/HourlyForecast/hourlyForcast.js
+++ src/scripts/HourlyForecast/hourlyForcast.js
@@ -1,24 +1,39 @@
 import './hourly-forecast.css';
+import { format, parse } from 'date-fns';
 
 //see description of { block: 'nearest' } here: https://stackoverflow.com/a/48635751/628418
 
-export default function makeHours() {
-	const hourCards = Array.from(document.querySelectorAll('.hour-card'));
+export default function makeHours(time, rawHourlyData) {
+	const currentHour = parseInt(format(new Date(), 'h'));
+	let meridiem = format(new Date(), 'bbb');
+
+	const hourCards = document.querySelectorAll('.hour-card');
+	const hourCardsArr = Array.from(hourCards);
 	const carousel = document.querySelector('.carousel');
 	const carouselWrapper = document.querySelector('.carousel-wrapper');
+
+	const times = carousel.querySelectorAll('.time');
+	times.forEach((time, i) => {
+		let hour = (currentHour + i) % 12;
+		if (hour === 0) {
+			hour = 12;
+			meridiem = meridiem === 'am' ? 'pm' : 'am';
+		}
+		time.textContent = `${hour}${meridiem}`;
+	});
 
 	function shiftCarouselBackward(cardWidth) {
 		const numberOfCards = Math.round(carousel.scrollLeft / cardWidth);
 
 		if (numberOfCards - 2 <= 0) {
-			hourCards[0].scrollIntoView({ block: 'nearest' });
+			hourCardsArrArr[0].scrollIntoView({ block: 'nearest' });
 			return;
 		}
 
 		const decimalPortion = parseFloat('0.' + (carousel.scrollLeft / cardWidth + '').split(/\D/)[1]);
 		decimalPortion >= 0.6
-			? hourCards[numberOfCards - 2].scrollIntoView({ block: 'nearest' })
-			: hourCards[numberOfCards - 1].scrollIntoView({ block: 'nearest' });
+			? hourCardsArr[numberOfCards - 2].scrollIntoView({ block: 'nearest' })
+			: hourCardsArr[numberOfCards - 1].scrollIntoView({ block: 'nearest' });
 	}
 
 	function shiftCarouselForward(cardWidth) {
@@ -26,11 +41,11 @@ export default function makeHours() {
 		const numberOfCards = carouselRightEdge / cardWidth;
 		const wholeNumber = parseFloat((numberOfCards + '').split(/\D/).at(0));
 
-		if (wholeNumber >= hourCards.length - 1) {
-			hourCards[hourCards.length - 1].scrollIntoView({ block: 'nearest' });
+		if (wholeNumber >= hourCardsArr.length - 1) {
+			hourCardsArr[hourCardsArr.length - 1].scrollIntoView({ block: 'nearest' });
 			return;
 		} else {
-			hourCards.at(wholeNumber + 1).scrollIntoView({ block: 'nearest' });
+			hourCardsArr.at(wholeNumber + 1).scrollIntoView({ block: 'nearest' });
 		}
 	}
 
@@ -41,7 +56,7 @@ export default function makeHours() {
 
 		const targetClasses = target.classList;
 		const carouselColGap = 10; //px
-		const cardWidth = hourCards[0].offsetWidth + carouselColGap;
+		const cardWidth = hourCardsArr[0].offsetWidth + carouselColGap;
 
 		const directionMapping = {
 			backward: () => shiftCarouselBackward(cardWidth),
