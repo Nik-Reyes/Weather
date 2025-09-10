fd20c6e2c98f1308f494667f7a0a61071674925c
“Nik <nikolaspreyes@gmail.com>
2025-09-09 19:54:22 -0700

    styles: add fonts $ tweak styles

--- src/main.js
+++ src/main.js
@@ -1,27 +1,29 @@
-import './style.css';
 import WeatherData from './modules/WeatherDataService.js';
-
 import makeHours from './scripts/HourlyForecast/hourlyForcast.js';
 import loadCurrentForcast from './scripts/CurrentForecast/currentForecast.js';
 import tenDayForecast from './scripts/TenDayForecast/tenDayForecast.js';
+import './style.css';
+
+function populateData() {
+	new Promise(resolve => {
+		resolve(weatherData.fetchWeatherData());
+	}).then(() => {
+		loadCurrentForcast(
+			weatherData.currentConditions,
+			weatherData.conditionDescription,
+			weatherData.minTemp,
+			weatherData.maxTemp,
+			weatherData.unit,
+		);
+		makeHours(weatherData.getHours(2)); // accepts days as param
+		tenDayForecast(weatherData.getDays(10));
+		pageWrapper.removeAttribute('style');
+	});
+	console.log('interval');
+}
 
 const pageWrapper = document.querySelector('.page-wrapper');
-
 // ask weather data service to get the weather data
 const weatherData = new WeatherData();
-new Promise(resolve => {
-	resolve(weatherData.fetchWeatherData());
-}).then(() => {
-	console.log(weatherData);
-
-	loadCurrentForcast(
-		weatherData.currentConditions,
-		weatherData.conditionDescription,
-		weatherData.minTemp,
-		weatherData.maxTemp,
-		weatherData.unit,
-	);
-	makeHours(weatherData.getHours(2)); // accepts days as param
-	tenDayForecast(weatherData.getDays(10));
-	pageWrapper.removeAttribute('style');
-});
+populateData();
+setInterval(populateData, 900000);
--- src/scripts/TenDayForecast/tenDayForecast.js
+++ src/scripts/TenDayForecast/tenDayForecast.js
@@ -7,7 +7,13 @@ export default function tenDayForecast(tenDayForecast) {
 	const dates = document.querySelectorAll('.ten-day-forecast .ten-day-card-title');
 
 	for (let i = 0; i < highs.length; i++) {
-		dates[i].textContent = format(parseISO(tenDayForecast[i].datetime), 'ccc do');
+		if (i === 0) {
+			dates[i].textContent = 'Today';
+		} else if (i === 1) {
+			dates[i].textContent = 'Tomorrow';
+		} else {
+			dates[i].textContent = format(parseISO(tenDayForecast[i].datetime), 'ccc do');
+		}
 		highs[i].textContent = `${parseInt(tenDayForecast[i].tempmax)}°`;
 		lows[i].textContent = `${parseInt(tenDayForecast[i].tempmin)}°`;
 	}
